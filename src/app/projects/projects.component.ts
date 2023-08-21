c576d67ef1b984af1ba4f3c01de711465bf30c7f
Sebaburst <sebarem723@gmail.com>
2023-08-20 23:38:00 -0400

    Links

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  redireccionarBike(): void {
+    window.open('https://bicicleteroutal.tech/', '_blank');
+  }
+  redireccionarDrosucatch(): void {
+    window.open('https://drosucatch.live/', '_blank');
+  }
+  redireccionarMobile(): void {
+    window.open('https://drive.google.com/uc?id=1VVBD_lSU38RmjesY1eL3OeBPH9G4ERJW&export=download', '_blank');
+  }
+
+  redireccionarStudents(): void {
+    window.open('https://tec-web-2022.web.app/', '_blank');
+  }
+
+  redireccionarRepo(): void {
+    window.open('https://drive.google.com/drive/folders/1uajUW4q_-NQyadS74-VEutYFa2uMWNfY?usp=drive_link', '_blank');
+  }
+
 
 }
